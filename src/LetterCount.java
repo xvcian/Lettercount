d3f4ccfd2a68bd47e092cf95adc64a4df0499825
xvcian <43534836+xvcian@users.noreply.github.com>
2018-11-30 13:35:16 +0800

    Update LetterCount.java

--- src/LetterCount.java
+++ src/LetterCount.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 public class LetterCount {
-        public static void main(String argv[])throws Exception{
+        public static void main(String args[])throws Exception{
             File fileSrc=new File("/home/lulu/hamlet.txt.html");
             BufferedReader fin=new BufferedReader( new FileReader(fileSrc) );
             String line;
